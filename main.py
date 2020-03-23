97c94b24ae5978e02d50d7d6d640156006145490
Subbie <subliminalmoment@gmail.com>
2020-03-23 17:58:36 +0200

    Change data source to real-time API

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import discord
-import csv
 import datetime
+import COVID19Py
+
+covid19 = COVID19Py.COVID19()
 
 client = discord.Client()
+
+locations = covid19.getLocations()
+   
 
 @client.event
 async def on_ready():
@@ -16,34 +21,34 @@ async def on_message(message):
         return
 
     if message.content.startswith('/corona'):
-        with open('03-22-2020.csv', 'r') as csv_file:
-            reader = csv.DictReader(csv_file)
-            
-            try:
-                for row in reader:
-                    if row['Province/State'] != "" and row['Province/State'] != row['Country/Region']:
-                        continue
-                    if row['Country/Region'].lower() == locationQuery:
-                        cases = row['Country/Region'] + " has **" + row['Confirmed'] + "** confirmed cases."
-                        await message.channel.send(cases)
-                        
-                        lastupdate = row["Last Update"]
-                        newdate = datetime.datetime.strptime(lastupdate, '%Y-%m-%dT%H:%M:%S')
-                        timedesc = "This information was updated on {:%A, %B %d, %Y}."
-                        timedesc = timedesc.format(newdate.date())
+        try:
+            for i in locations:
+                if i['province'] != "":
+                    continue
+                if i['country'].lower() == locationQuery:
+                    country = str(i['country'])
+                    confirmed = str(i['latest']['confirmed'])
 
-                        deaths = row["Deaths"]
-                        recovered = row["Recovered"]
+                    cases = country + " has **" + confirmed + "** confirmed cases."
+                    await message.channel.send(cases)
+                
+                    lastupdate = str(i['last_updated'])
+                    newdate = datetime.datetime.strptime(lastupdate, '%Y-%m-%dT%H:%M:%S.%fZ')
+                    timedesc = "This information was updated on {:%A, %B %d, %Y}."
+                    timedesc = timedesc.format(newdate.date())
 
-                        embed = discord.Embed(title="Coronavirus cases in " + row["Country/Region"], description=timedesc, color=0x764BA7)
-                        embed.add_field(name="Deaths", value=deaths, inline=True)
-                        embed.add_field(name="Recovered", value=recovered, inline=True)
-                        imgurl = "https://i.imgur.com/UrbDTj1.png"
-                        embed.set_thumbnail(url=imgurl)
-                        await message.channel.send(embed=embed)
-                    
-            except Exception as ex:
-                print(ex)
+                    deaths = str(i['latest']['deaths'])
+                    recovered = str(i['latest']['recovered'])
+
+                    embed = discord.Embed(title="Coronavirus cases in " + country, description=timedesc, color=0x764BA7)
+                    embed.add_field(name="Deaths", value=deaths, inline=True)
+                    embed.add_field(name="Recovered", value=recovered, inline=True)
+                    imgurl = "https://i.imgur.com/UrbDTj1.png"
+                    embed.set_thumbnail(url=imgurl)
+                    await message.channel.send(embed=embed)
+                
+        except Exception as ex:
+            print(ex)
 
 if __name__ == '__main__':
     import config
